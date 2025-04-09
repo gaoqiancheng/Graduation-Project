910a29186ceda5fd024f623165be9306db7816fa
Immutable <2513676981@qq.com>
2025-04-09 17:33:22 +0800

    创建了前端项目

---
+++ web-app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app" class="app-container">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import '@/assets/styles/global.css'
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
