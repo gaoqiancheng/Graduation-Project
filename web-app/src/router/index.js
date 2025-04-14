220abf8ceb4415f4de76078f76647387936bb2ca
Immutable <2513676981@qq.com>
2025-04-14 16:03:56 +0800

    服务端初始化

--- web-app/src/router/index.js
+++ web-app/src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '../views/HomePage.vue'
+import HomePage from '@/views/HomePage.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'HomePage',
+    name: 'Home',
     component: HomePage
   }
 ]
